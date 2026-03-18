536f3dbcd6717e957cba68ea1a1ccb5a48e36dd7
Laly Robyr <laly.robyr@epfl.ch>
2026-03-18 20:18:23 +0100

    modify text based on Som comments

--- data.js
+++ data.js
@@ -40,6 +40,15 @@ const TEAM_MEMBERS = [
     linkedin: 'https://www.linkedin.com/in/sebastien-guilmot/', 
     email: null 
   },
+  { 
+    name: 'Sofia Domingues',          
+    role: 'Head of Business Development',                                
+    rank: 5, 
+    initials: 'SD',  
+    photo: 'assets/team/Sofia.jpg',                         
+    linkedin: 'https://www.linkedin.com/in/sofiamelodomingues/', 
+    email: null 
+  },
 ];
 
 const COLLABORATORS = [
@@ -62,6 +71,13 @@ const BOARD_MEMBERS = [
 
 const PUBLICATIONS = [
   {
+    journal: 'Nature Communications',
+    title:   'Miniature neurotransmission is required to maintain Drosophila synaptic structures during ageing',
+    excerpt: 'Through discrete manipulation of each neurotransmission modality, we find that miniature but not evoked neurotransmission is required to maintain presynaptic architecture and that increasing miniature events can both preserve synaptic structures and prolong motor ability during ageing',
+    year:    '2021',
+    doi:     'https://doi.org/10.1038/s41467-021-24490-1',
+  },
+  {
     journal: 'Cell Reports',
     title:   'Trio preserves motor synapses and prolongs motor ability during aging',
     excerpt: 'We demonstrate that increasing Trio expression in adult Drosophila can abrogate age-dependent synaptic structural fragmentation, postpone the decline of motor ability, and maintain the capacity of motor synapses to sustain high-intensity neurotransmitter release.',
@@ -69,15 +85,8 @@ const PUBLICATIONS = [
     doi:     'https://doi.org/10.1016/j.celrep.2024.114256',
   },
   {
-    journal: 'Nature Communications',
-    title:   'Miniature neurotransmission is required to maintain Drosophila synaptic structures during ageing',
-    excerpt: 'Our results establish that miniature neurotransmission, formerly viewed as an epiphenomenon, is necessary for the long-term stability of synaptic connections.',
-    year:    '2021',
-    doi:     'https://doi.org/10.1038/s41467-021-24490-1',
-  },
-  {
-    journal: 'TheScientist',
-    title:   'Combating Age-Related Motor Decline',
+    journal: 'The Scientist',
+    title:   'Combating Age-Related Motor Decline: <br> Increasing levels of the protein Trio preserved synaptic function and motor abilities in aging flies.',
     excerpt: 'When [the researchers] genetically decreased Trio levels, synaptic boutons broke up much more rapidly as the flies got older. When they overexpressed the protein, the synaptic structures were almost entirely preserved',
     year:    '2024',
     doi:     'https://www.the-scientist.com/combating-age-related-motor-decline-72095',
